c3382b39c3fa35c63507f8ade554a905667ce6a2
Oscar Bonilla <oscar.bonillagamboa@cgi.com>
2018-07-17 15:53:42 -0400

    Add Angular pages

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const app = express()
 
 const PORT = process.env.PORT || 3001
 
-app.use(express.static(path.join(__dirname,'frontend','public')));
-app.set('views', path.join(__dirname, 'frontend'));
-app.set('view engine', 'ejs');
+// app.use(express.static(path.join(__dirname,'frontend','public')));
+// app.set('views', path.join(__dirname, 'frontend'));
+// app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname,'sprintwebapp','dist','sprintwebapp')));
+app.set('views', path.join(__dirname,'sprintwebapp','dist'));
+
 
 app.get('/', function (req, res) {
   //res.send('Hello World!')
-  res.render('pages/index');
+  res.render('sprintwebapp/index');
 })
 
 app.listen(PORT, function () {
